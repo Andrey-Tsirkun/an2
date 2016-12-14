646aa58c2bef7d791483085d84a9e380ac187b8f
Andrey Tsirkun <andrey_tsirkun@epam.com>
2016-12-14 18:52:21 +0300

    3

--- app/pipes/cityweather.pipe.ts
+++ app/pipes/cityweather.pipe.ts
@@ -23,70 +23,33 @@ interface CityTemp {
 })
 export class CityweatherPipe implements PipeTransform {
     output: Observable<CityTemp>;
-    cachedNames: string[];
-    cityCache: CityTemp[] = [];
+    cache: Map<string, Object>;
 
-    constructor(private http: Http) {}
+    constructor(private http: Http) {
+        this.cache = new Map<string,Object>();
+    }
 
     transform(city: string): Observable<CityTemp> {
 
-        let index: number;
-        this.cachedNames = this.cityCache.map(n => n.name);
-        index = this.cachedNames.indexOf(city);
-        console.log(this.cachedNames, this.cityCache, index)
-
-        if (index === -1) {
-            this.output = this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${config.id}`)
-                .map(newCity => {
-                    console.log('newCity', JSON.parse(newCity["_body"]))
-                    this.cityCache.push(JSON.parse(newCity["_body"]));
-                    return newCity;
-                })
-        }
-        else {
-            const cachedIndex = this.cityCache[index];
-            const name = cachedIndex.name;
-            const temp = cachedIndex.main.temp;
-
-            this.output = new Observable(
-                (observer: Subscriber<CityTemp>) => {
-                    observer.next(`${name}: current temperature is ${temp}°C`);
-                });
-        }
-
-        return this.output;
-    }
-
-            /*if(this.model.hasOwnProperty(value)) {
-                return new Promise(()=> {})
+        if(city) {
+            if (!this.cache.hasOwnProperty(city)) {
+                this.output = this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${config.id}`)
+                    .map(data => {
+                        let shortData = JSON.parse(data["_body"]);
+                        this.cache[city] = shortData;
+                        return shortData;
+                    })
             }
             else {
-                return this.get(`http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${config.id}`).then((res) => {
-                    let data = JSON.parse(res.text);
+                let cityData = this.cache[city]
 
-                    cityName = data.name;
-                    temp = new KelvintocelsiumPipe().transform(data.main.temp);
-                    humidity = data.main.humidity;
-                    pressure = data.main.pressure;
-                    wind = data.wind.speed;
+                this.output = new Observable(
+                    (observer: Subscriber<CityTemp>) => {
+                        observer.next(cityData);
+                    });
+            }
 
-                    this.model[cityName] = {
-                        temp: new KelvintocelsiumPipe().transform(data.main.temp),
-                        humidity: data.main.humidity,
-                        pressure: data.main.pressure,
-                        wind: data.wind.speed,
-                        stamp: + new Date()
-                    };
-
-                    return `<div>${cityName}</div>
-                            <div><span>Temperature:</span> ${temp}°</div>
-                            <div><span>Humidity:</span> ${humidity}%</div>
-                            <div><span>Pressure:</span> ${pressure} gPa</div>
-                            <div><span>Wind:</span> ${wind} m/s</div>`;
-                }, (err) => {
-                    return `<div>Something went wrong!</div>
-                            <div>err</div>`
-                });
-            }*/
-
+            return this.output;
+        }
     }
+}
