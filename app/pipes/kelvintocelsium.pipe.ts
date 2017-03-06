97a68e87267a4af1d7846d61ef19659ddcf1b92a
Andrey Tsirkun <andrey_tsirkun@epam.com>
2017-03-06 11:29:50 +0300

    rxjs Dummy app

--- app/pipes/kelvintocelsium.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-@Pipe({
-    name: 'kelvintocelsium'
-})
-export class KelvinToCelsiumPipe implements PipeTransform {
-    transform(value: number): number {
-        return +(value - 273.15).toFixed(1);
-    }
-}
--- app/vendor.ts
+++
@@ -1,9 +0,0 @@
-import 'reflect-metadata';
-import 'rxjs';
-import 'zone.js/dist/zone';
-
-import '@angular/common';
-import '@angular/compiler';
-import '@angular/core';
-import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
